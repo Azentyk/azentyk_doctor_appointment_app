e35ebad488f0613395e4cfe8d489189096936cf2
SundaresanAI <ss@azentyk.ai>
2025-09-16 19:49:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,8 +42,10 @@ def create_app():
     return app
 
 
+app = create_app()
+
 # Azure entry point
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))  # Azure sets PORT env var
     app.run(host="0.0.0.0", port=port, debug=True)
+
